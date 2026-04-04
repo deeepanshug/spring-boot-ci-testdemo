a026dde9bacfec9a330c65406084a0ac220fd3b0
DeeePG <dg35534@gmail.com>
2026-04-04 21:12:47 +0530

    update

---
+++ src/main/java/com/hello/helloapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.hello.helloapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    private String hello() {
+        return "Hello from CI/CD pipeline.";
+    }
+}
